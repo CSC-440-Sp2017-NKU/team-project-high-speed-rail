975892749a036d4b80afa69389ab387bef59d92d
EJ Schroeder <schroedere1@nku.edu>
2017-03-23 23:44:53 +0000

    Fix remember me, removed line that was supposed to be deleted

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
