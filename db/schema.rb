e25bf1bb683711c37a3d91254816a441779f7c0e
EJ Schroeder <schroedere1@nku.edu>
2017-03-23 21:36:26 +0000

    Complete user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323004540) do
+ActiveRecord::Schema.define(version: 20170323202703) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "content"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170323004540) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
