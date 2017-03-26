f12a79ac0ad50d141f2a06c2c3879366987c6252
EJ Schroeder <schroedere1@nku.edu>
2017-03-26 03:08:12 +0000

    Add friendly redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < ActionController::Base
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
